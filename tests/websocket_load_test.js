3acc8594781852c61181d800785004f007edeae4
Mrcoder420 <nikhilbhor201@gmail.com>
2026-05-24 23:04:22 +0530

    test: make RAMP_UP_MS configurable in stress test

--- tests/websocket_load_test.js
+++ tests/websocket_load_test.js
@@ -15,7 +15,7 @@ const { generateAccessToken } = require('../src/utils/jwt');
 // Arguments
 const CONCURRENCY = parseInt(process.argv[2], 10) || 500;
 const TARGET      = process.argv[3] || 'http://54.91.119.137';
-const RAMP_UP_MS  = 10; // Connect 1 socket every 10ms (100 sockets per second)
+const RAMP_UP_MS  = parseInt(process.argv[4], 10) || 10; 
 
 console.log(`\n🔥 ELEVORA WEBSOCKET CONCURRENCY LOAD TEST`);
 console.log(`=========================================`);
