ee501065ff5cd75470745cad40c10be6c7051af0
Mrcoder420 <nikhilbhor201@gmail.com>
2026-07-03 14:31:57 +0530

    fix: resolve undefined middleware import (protect -> authenticate) causing server crash on startup

--- src/routes/telemetryRoutes.js
+++ src/routes/telemetryRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middlewares/authMiddleware');
+const { authenticate } = require('../middlewares/authMiddleware');
 const telemetryController = require('../controllers/telemetryController');
 
 // All telemetry routes require a standard logged-in user
-router.get('/ab-tests', protect, telemetryController.getActiveTests);
-router.post('/events', protect, telemetryController.recordEvent);
-router.post('/feedback', protect, telemetryController.submitFeedback);
+router.get('/ab-tests', authenticate, telemetryController.getActiveTests);
+router.post('/events', authenticate, telemetryController.recordEvent);
+router.post('/feedback', authenticate, telemetryController.submitFeedback);
 
 module.exports = router;
 
